556a214e196619edb3e84e199e0a9f6da4c0646c
hongxeob <sjun2918@naver.com>
2022-11-06 23:51:06 +0900

    오류로 인한 레파지토리 재 생성

--- src/chap2/CloneArray.java
+++ src/chap2/CloneArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CloneArray {
     // 배열을 복제
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6};
+        int[] a = {1, 2, 3, 4, 5};
         int[] b = a.clone();
         b[4] = 0;
         System.out.println("a=" + Arrays.toString(a));
